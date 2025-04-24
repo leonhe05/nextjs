dabb1d71e21fa717f6c1ecc22a2a3115aa782cfb
heyaxing <1123640216@qq.com>
2025-04-24 09:12:11 +0800

    增加描述标签

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "I Speaker | 专业有声书制作",
-  description: "AI文本转语音|AI自动多角色配音|AI超拟人音色",
+  description: "AI文本转语音|AI自动多角色配音|AI超拟人音色|AI语音合成|配音|TTS|文本转语音|语音合成",
   icons: {
     icon: '/logo.png',
   },
